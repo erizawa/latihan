8bedecd0bc055e28bb68247c97f4fa0397784695
Muhammad Riza <muhriza66@gmail.com>
2018-06-27 16:51:25 +0700

    Create moontown-simpen-link.js

---
+++ adsafe-js/moontown-simpen-link.js
@@ -0,0 +1,28 @@
+<script>
+//<![CDATA[
+var seconds = 10; // Berapa detik waktu mundurnya
+function generate() { // Nama fungsi countdownnya
+    var id;
+    id = setInterval(function() {
+        if (seconds < 1) {
+            clearInterval(id);
+
+            var cek_id = getParameter("hash");
+            var decrypt_url = "https://moontown.simpen.link/p/skip.html?hash=" // domain user
+            if (cek_id == false) {
+                alert("URL Tidak Valid!")
+            } else if (cek_id == "undefined") {
+                alert("URL Tidak Valid!")
+            } else {
+                var id_decoded = decrypt_url + cek_id;
+                $('#btn-lanjutkan').html("Lanjutkan &raquo;");
+                $('#btn-lanjutkan').removeClass("disabled");
+                $('#btn-lanjutkan').attr("href", id_decoded);
+            }
+        } else {
+            $('#tunggu').html(--seconds);
+        }
+    }, 1000);
+}
+//]]>
+</script>
